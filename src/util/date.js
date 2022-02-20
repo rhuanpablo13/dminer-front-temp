2fe3daef09599f243433f69e18aa83e7d10a5b4b
Andressa Novaes <isa.desenvolvimento@gmail.com>
2022-02-20 20:17:21 -0300

    feat(calendar): ajuste date

--- src/util/date.js
+++ src/util/date.js
@@ -8,7 +8,7 @@ export const dateNowFormart = () => {
 }
 
 export const dateHourFormart = (date) => {
-  return moment(date).format('DD/MM/YYYY hh:mm')
+  return moment(date).format('DD/MM/YYYY HH:mm')
 }
 
 export const mounthCurrent = () => new Date().getMonth() + 1
@@ -18,7 +18,7 @@ export const dateFormart = (date) => moment(date).format(FORMAT_DATE)
 export const dayMounthFormart = (date) => moment(date).format('DD/MM')
 
 export const dateHourFormarUs = (date) =>
-  moment(date).format('YYYY-MM-DD hh:mm:ss')
+  moment(date).format('YYYY-MM-DD HH:mm:ss')
 
 export const diffDate = (date1, date2) =>  moment(date2,"DD/MM/YYYY HH:mm:ss").diff(moment(date1,"DD/MM/YYYY HH:mm:ss"))
 
