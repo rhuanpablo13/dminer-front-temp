f094feddaf673efe877c936fedd898b3158af0cf
Andressa Novaes <isa.desenvolvimento@gmail.com>
2022-02-27 13:17:43 -0300

    feat: ajuste de http

--- src/api/http.js
+++ src/api/http.js
@@ -17,7 +17,7 @@ export const wbDiminerImage = axios.create({
 })
 
 export const apiIntra = axios.create({
-  baseURL: 'https://dminer-api.herokuapp.com/api/',
+  baseURL: 'http://localhost:8081/api/', //'https://dminer-api.herokuapp.com/api/',
   headers: {
     'Content-Type': 'application/json'
   }
