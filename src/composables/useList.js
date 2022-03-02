80d7441f3e7ad15bec9937e621ba7044af6f1de5
C1321644 Isa Andressa Novaes Menezes <c13216440@interno.bb.com.br>
2022-03-02 16:35:00 -0300

    fix(favorites): ajuste

--- src/composables/useList.js
+++ src/composables/useList.js
@@ -1,8 +1,8 @@
 import { ref } from 'vue'
 import { fetchAll, fetchCreate, fetchUpdate, fetchDelete, fetchSearch,fetchDropdown, fetchFind, fetchSearchItem} from '@/api/request.js'
 import { fetchQuizAnswer } from '@/api/survey.js'
-import { fetchSearchAll, fetchCreateFavorite } from '@/api/feed.js'
-import { fetchAllFavorites } from '@/api/favorite'
+import { fetchSearchAll } from '@/api/feed.js'
+import { fetchAllFavorites, fetchCreateFavorite } from '@/api/favorite'
 
 export default function useList() {
   const getListItem = ref([])
